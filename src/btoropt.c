95a42c96abadc0fd31981f724cb44affb448f3c2
Aina Niemetz <aina.niemetz@gmail.com>
2019-04-26 11:29:26 -0700

    Add option to enable/disable ZERO_LOWER_SLICE rw rule.

--- src/btoropt.c
+++ src/btoropt.c
@@ -1351,6 +1351,16 @@ btor_opt_init_opts (Btor *btor)
             0,
             1,
             "update current model w.r.t. synthesized skolem function");
+  init_opt (btor,
+            BTOR_OPT_RW_ZERO_LOWER_SLICE,
+            true,
+            true,
+            "rw-zero-lower-slice",
+            0,
+            0,
+            0,
+            1,
+            "enable zero_lower_slice rewrite");
 }
 
 void
